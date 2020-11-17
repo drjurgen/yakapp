8b98e27bc62a91fd6f912afb6a04b2a712487e14
drjurgen <nicoj98@gmail.com>
2020-11-17 12:10:55 +0100

    added database fetch

--- src/App.js
+++ src/App.js
@@ -1,76 +1,68 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import Main from "./components/Main";
-import './App.css';
+import "./App.css";
 
 function App() {
-  const [cards, setCards] = useState([
-    {
-      task:"Clean room",
-      list:"todo",
-      highlight:true
-    },
-    {
-      task:"Clean kitchen",
-      list:"doing",
-      highlight:false
-    },
-    {
-      task:"Do JS",
-      list:"done",
-      highlight:true
-    },
-    {
-      task:"Learn React",
-      list:"doing",
-      highlight:false
-    }
-  ]);
-  
+  useEffect(() => {
+    fetch("https://reactstuff1-f933.restdb.io/rest/cards", {
+      method: "get",
+      headers: {
+        "Content-type": "application/json; charset=utf-8",
+        "x-apikey": "5fb3ac4b863959728838545a",
+        "cache-control": "no-cache",
+      },
+    })
+      .then((e) => e.json())
+      .then((data) => {
+        setCards(data);
+      });
+  }, []);
+
+  const [cards, setCards] = useState([]);
+
   const day = "monday".split("").join(" ");
   const message = "Hi Mom";
   const addCard = () => {
-    setCards(cards.concat({
-      task:"Learn React " + Math.random(),
-      list:["todo", "doing", "done"][Math.floor(Math.random()*3)],
-      highlight:false
-    }))
-  }
+    setCards(
+      cards.concat({
+        task: "Learn React " + Math.random(),
+        list: ["todo", "doing", "done"][Math.floor(Math.random() * 3)],
+        highlight: false,
+      })
+    );
+  };
   return (
     <>
-    <h1>Hello {`my friend it's ${day}`}</h1>
-    <button onClick={addCard}>Add Card</button>
-    <div className="App">
-      <Nav />
-      
-      <Main cards={cards} message={message}/>
-    </div>
+      <h1>Hello {`my friend it's ${day}`}</h1>
+      <button onClick={addCard}>Add Card</button>
+      <div className="App">
+        <Nav />
+
+        <Main cards={cards} message={message} />
+      </div>
     </>
   );
 }
 
-function Nav(){
-  const [items, setItems] = useState([
-    "hi","mom","hotpink"
-  ])
-  function addItem(){
+function Nav() {
+  const [items, setItems] = useState(["hi", "mom", "hotpink"]);
+  function addItem() {
     //create a copy
-    const nextState = items.concat("Dannie")
+    const nextState = items.concat("Dannie");
 
-    setItems(nextState)
+    setItems(nextState);
   }
 
   return (
     <nav className="Nav">
       <h2>Hi im nav </h2>
-      {
-        items.map((item, index)=><p key={index} >{item}</p>)
-      }
-      
-      
+      {items.map((item, index) => (
+        <p key={index}>{item}</p>
+      ))}
+
       <button onClick={addItem}>Click me</button>
     </nav>
-  )
+  );
 }
-
 
 export default App;
--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -1,19 +1,23 @@
-
 import React from "react";
 
 import Card from "./Card";
 import AddCardForm from "./AddCardForm";
-export default function ListContainer(props){
-  const form = props.showForm ? <AddCardForm message={props.message} />:null
-    return (
-      <section>
-        <h1>{props.name}</h1>
-        {props.cards.map(card=>{
-          return <Card task={card.task} highlight={card.highlight} list={card.list} />
-        })}
-        {form}
-        
-
-      </section>
-    )
-  }
+export default function ListContainer(props) {
+  const form = props.showForm ? <AddCardForm message={props.message} /> : null;
+  return (
+    <section>
+      <h1>{props.name}</h1>
+      {props.cards.map((card) => {
+        return (
+          <Card
+            key={card._id}
+            task={card.task}
+            highlight={card.highlight}
+            list={card.list}
+          />
+        );
+      })}
+      {form}
+    </section>
+  );
+}
